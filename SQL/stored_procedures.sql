5e9150b49e1493048a1ff2f247b7dda9c97434e4
satyamsingh201 <110818377+satyamsingh201@users.noreply.github.com>
2026-04-19 03:02:37 +0530

    Add stored procedure to update water_table

--- SQL/stored_procedures.sql
+++ SQL/stored_procedures.sql
@@ -1,1 +1,12 @@
 
+CREATE PROCEDURE Update_water_table
+  @Last_load VARCHAR(200)
+
+AS
+BEGIN
+
+   BEGIN TRANSACTION
+    UPDATE water_table
+    SET Last_load=@Last_load
+    COMMIT TRANSACTION
+END;
