f5215a8e5920578cc0176eca4df79c7b4c0523e5
satyamsingh201 <110818377+satyamsingh201@users.noreply.github.com>
2026-04-19 03:02:03 +0530

    Update create_tables.sql

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,1 +1,31 @@
+DROP TABLE IF EXISTS source_car_data;
+CREATE TABLE source_car_data (
+    Branch_ID      VARCHAR(200),
+    Dealer_ID      VARCHAR(200),
+    Model_ID       VARCHAR(200),
+    Revenue        BIGINT,
+    Units_Sold     BIGINT,
+    Date_ID        VARCHAR(200),
+    Day            INT,
+    Month          INT,
+    Year           INT,
+    BranchName     VARCHAR(200),
+    DealerName     VARCHAR(200),
+    Product_Name   VARCHAR(200)
+);
 
+SELECT * FROM source_car_data
+
+SELECT MIN(Date_ID) FROM source_car_data
+
+
+CREATE TABLE water_table
+(
+    Last_load  VARCHAR(200),
+);
+
+
+INSERT INTO water_table(Last_load)
+VALUES('DT0000')
+
+SELECT * FROM water_table;
